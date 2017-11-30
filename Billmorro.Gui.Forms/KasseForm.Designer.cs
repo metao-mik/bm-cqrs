d1f0f3027247cceb0a4dc7f8b13a4de34f6c5482
META-OBJECTS\mklei <michael.klei@meta-objects.net>
2017-11-30 17:39:37 +0100

    Windows Forms für BarcodeHinzufügen ergänzt.

---
+++ Billmorro.Gui.Forms/KasseForm.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace Billmorro.Gui.Forms
+{
+    partial class KasseForm
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.BarcodeTextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.bonIdTextbox = new System.Windows.Forms.TextBox();
+            this.AnzahlPosTextbox = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(490, 86);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(172, 240);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.textBox4);
+            this.panel2.Controls.Add(this.AnzahlPosTextbox);
+            this.panel2.Controls.Add(this.bonIdTextbox);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(268, 482);
+            this.panel2.TabIndex = 3;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.BarcodeTextBox);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(268, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(464, 72);
+            this.panel1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Barcode";
+            // 
+            // BarcodeTextBox
+            // 
+            this.BarcodeTextBox.Location = new System.Drawing.Point(90, 13);
+            this.BarcodeTextBox.Name = "BarcodeTextBox";
+            this.BarcodeTextBox.Size = new System.Drawing.Size(100, 22);
+            this.BarcodeTextBox.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(196, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Scann";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // bonIdTextbox
+            // 
+            this.bonIdTextbox.Location = new System.Drawing.Point(96, 36);
+            this.bonIdTextbox.Name = "bonIdTextbox";
+            this.bonIdTextbox.Size = new System.Drawing.Size(100, 22);
+            this.bonIdTextbox.TabIndex = 0;
+            // 
+            // AnzahlPosTextbox
+            // 
+            this.AnzahlPosTextbox.Location = new System.Drawing.Point(96, 64);
+            this.AnzahlPosTextbox.Name = "AnzahlPosTextbox";
+            this.AnzahlPosTextbox.Size = new System.Drawing.Size(100, 22);
+            this.AnzahlPosTextbox.TabIndex = 1;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(96, 8);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Bon ID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "anzahl Pos";
+            // 
+            // KasseForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(732, 482);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "KasseForm";
+            this.Text = "Billmorro Kasse ";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox BarcodeTextBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox AnzahlPosTextbox;
+        private System.Windows.Forms.TextBox bonIdTextbox;
+    }
+}
+
---
+++ Billmorro.Gui.Forms/KasseForm.cs
@@ -0,0 +1,62 @@
+﻿using Billmorro.ClientApi.Kasse;
+using Billmorro.Implementierung;
+using Billmorro.Infrastruktur.Implementierung;
+using Billmorro.Infrastruktur.Reactive;
+using Billmorro.ModulApi.Geraete;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Billmorro.Gui.Forms
+{
+    public partial class KasseForm : Form
+    {
+        private KasseClientApi _clientapi;
+        private KasseQueryApi _queryapi;
+        private Bon _aktuellerBon;
+
+        public KasseForm()
+        {
+            InitializeComponent();
+            Startup();
+        }
+
+
+
+        private void Startup()
+        {
+            var eventstore = new InMemoryEventStore(() => DateTime.UtcNow);
+
+            var geraete = new Geraetemodul();
+            _clientapi = new KasseClientApi(
+                new ModulApi.Produkte.Produktmodul(),
+                geraete,
+                    new VerkaufCommandHandler(eventstore,
+                        ex => { throw new Exception("Fehler in Testausführung: " + ex.Message, ex); })
+                );
+
+            _queryapi = new KasseQueryApi(new VerkaufQuery(eventstore), geraete);
+
+            //_aktuellerBon = null;
+            _queryapi.AktuellerBon.Subscribe(ShowBon);
+        }
+
+        private void ShowBon(Bon bon)
+        {
+            bonIdTextbox.Text = bon.NettoBetrag.ToString();
+            AnzahlPosTextbox.Text = bon.Positionen.Count.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var barcode = BarcodeTextBox.Text;
+            _clientapi.Hinzufuegen_Barcode(barcode);
+        }
+    }
+}
